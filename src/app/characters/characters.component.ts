a7ab45a0b078aea3bf140f3f11331785914fd27a
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-06-05 00:18:46 -0500

    Characters
    Se realiza el componente para los pokemons

--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CharacterService } from '../characters.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { Character } from '../character';
 
 @Component({
   selector: 'app-characters',
@@ -7,9 +11,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharactersComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['id', 'name', 'image']
+  data: Character[] = []
+  dataSource = new MatTableDataSource<Character>(this.data)
+  pokemons = []
+
+  @ViewChild(MatPaginator, { static: true })
+  paginator!: MatPaginator;
+
+
+  constructor(private characterService: CharacterService) { }
 
   ngOnInit(): void {
+    this.getCharacters()
   }
 
+ getCharacters(){
+   let character;
+   for(let i = 1; i <= 150; i++){
+   this.characterService.getCharacters(i).subscribe(
+     res => {
+      character = {
+         id: i,
+         name: res.name,
+         image: res.sprites.front_default
+       };
+       this.data.push(character)
+       this.dataSource = new MatTableDataSource<Character>(this.data)
+       this.dataSource.paginator = this.paginator
+     },
+     err => {
+      console.log(err)
+     }
+   )}
+ }
+
+ applyFilter(event: Event) {
+  const filterValue = (event.target as HTMLInputElement).value;
+  this.dataSource.filter = filterValue.trim().toLowerCase();
+
+  if (this.dataSource.paginator) {
+    this.dataSource.paginator.firstPage();
+  }
+}
 }
