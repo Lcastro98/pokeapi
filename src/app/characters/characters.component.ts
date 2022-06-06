3ff4a04a6132b515ab9e332400c50ba187eccb96
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-06-05 23:44:06 -0500

    Reformateo
    Se reformatea el codigo

--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -9,50 +9,54 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-characters',
   templateUrl: './characters.component.html',
-  styleUrls: ['./characters.component.sass']
+  styleUrls: ['./characters.component.sass'],
 })
 export class CharactersComponent implements OnInit {
-
-  displayedColumns: string[] = ['id', 'name', 'image']
-  data: Character[] = []
-  dataSource = new MatTableDataSource<Character>(this.data)
-  pokemons = []
+  displayedColumns: string[] = ['id', 'name', 'image'];
+  data: Character[] = [];
+  dataSource = new MatTableDataSource<Character>(this.data);
+  pokemons = [];
 
   @ViewChild(MatPaginator, { static: true })
   paginator!: MatPaginator;
 
-
-  constructor(private characterService: CharacterService, private router: Router) { }
+  constructor(
+    private characterService: CharacterService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.getCharacters()
+    this.getCharacters();
   }
 
- getCharacters(){
-   let character;
-   range(1, 150).subscribe( id => 
-   this.characterService.getCharacters(id).subscribe(
-    (res) => {
-      character = {
-         id: id,
-         name: res.name,
-         image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
-       };
-       this.data.push(character)
-       this.dataSource = new MatTableDataSource<Character>(this.data)
-       this.dataSource.paginator = this.paginator
-    })
- )}
-
- applyFilter(event: Event) {
-  const filterValue = (event.target as HTMLInputElement).value;
-  this.dataSource.filter = filterValue.trim().toLowerCase();
-
-  if (this.dataSource.paginator) {
-    this.dataSource.paginator.firstPage();
+  getCharacters() {
+    let character;
+    range(1, 150).subscribe((id) =>
+      this.characterService.getCharacters(id).subscribe((res) => {
+        character = {
+          id: id,
+          name: res.name,
+          image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
+          base_experience: res.base_experience,
+          height: res.height,
+          weight: res.weight,
+        };
+        this.data.push(character);
+        this.dataSource = new MatTableDataSource<Character>(this.data);
+        this.dataSource.paginator = this.paginator;
+      })
+    );
   }
-}
-  getRow(row:Character){
-    this.router.navigateByUrl(`detail/${row.id}`)
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  getRow(row: Character) {
+    this.router.navigateByUrl(`detail/${row.id}`);
   }
 }
